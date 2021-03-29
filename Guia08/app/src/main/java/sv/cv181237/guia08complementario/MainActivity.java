0740f872a36831fb6100073130b3e3ff9603f4ff
Oscar Carranza <carranzav_oscar@hotmail.com>
2021-03-28 18:58:04 -0600

    Complementario RealTime Database

---
+++ Guia08/app/src/main/java/sv/cv181237/guia08complementario/MainActivity.java
@@ -0,0 +1,14 @@
+package sv.cv181237.guia08complementario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
